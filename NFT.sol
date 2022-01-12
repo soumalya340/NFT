71c42992ba45c6cbfd9ce507cce63881006da5ad
soumalya340 <54816468+soumalya340@users.noreply.github.com>
2022-01-12 22:39:20 +0530

    FIRST COMMIT

---
+++ NFT.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+//Import ERC1155 token contract from Openzeppelin
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract MyNftcontract is ERC1155,Ownable
+{
+    uint256 public constant ARTWORK = 0;
+    uint256 public constant PHOTO = 1;
+
+    constructor() ERC1155(""){
+        _mint(msg.sender,ARTWORK,1,"");
+        _mint(msg.sender,PHOTO,2,"");
+    }
+    function mint( address to,
+        uint256 id,
+        uint256 amount) public onlyOwner{
+        
+        _mint( to,id,amount,"");
+    }
+
+    function burn( address from,
+        uint256 id,
+        uint256 amount) public{
+            require(msg.sender == account);
+            _burn( from,id,amount);
+        }
+}
